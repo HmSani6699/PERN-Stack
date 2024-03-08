8012f59a88b74b8b987a3f32f304ef7ef9371788
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-03-08 21:59:21 +0600

    complet get api

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const cors = require("cors");
+
+// using the middleweare
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Todo Server is running on tha port: http://localhost:5000");
 });
 
 // Get all user
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    res.send({ message: "Get all users" });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+});
+
 // Get a user
 // Post a user
 // Update a user
