00c73bfe0a5446da42f34f54971f9d40cc7a2b83
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-03-11 07:33:06 +0600

    complete form page

--- todo-app/todo-Frontend/src/App.jsx
+++ todo-app/todo-Frontend/src/App.jsx
@@ -6,20 +6,43 @@ function App() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
-  return (
-    <>
-      <h1 className="text-4xl font-bold text-center mt-6">Simple Todo App</h1>
+  // console.log(name, phone, email);
 
-      <div>
-        <InputField
-          title="Name"
-          value={name}
-          setValue={setName}
-          type="text"
-          placeholder="Enter your name"
-        />
+  return (
+    <div className="max-w-6xl mx-auto">
+      <h1 className="text-4xl font-bold text-center mt-6 mb-10">
+        Simple Todo App
+      </h1>
+
+      <div className="flex items-center gap-4">
+        <div className="w-[90%] grid grid-cols-1 md:grid-clos-2 lg:grid-cols-3 gap-4 ">
+          <InputField
+            title="Name"
+            value={name}
+            setValue={setName}
+            type="text"
+            placeholder="Enter your name"
+          />
+          <InputField
+            title="Phone"
+            value={phone}
+            setValue={setPhone}
+            type="text"
+            placeholder="Enter your Phone number"
+          />
+          <InputField
+            title="Email"
+            value={email}
+            setValue={setEmail}
+            type="text"
+            placeholder="Enter your Email"
+          />
+        </div>
+        <button className="w-[10%] p-3 bg-green-400 text-white rounded-md border-none font-bold mt-[35px]">
+          +Add User
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
--- todo-app/todo-Frontend/src/component/InputField.jsx
+++ todo-app/todo-Frontend/src/component/InputField.jsx
@@ -3,12 +3,13 @@ import React from "react";
 const InputField = ({ value, setValue, type = "text", placeholder, title }) => {
   return (
     <div>
-      <p></p>
-
       {title && <p className="font-semibold mb-2 text-[18px]"> {title}</p>}
       <input
         className="w-full outline-none border-2 border-gray-300 rounded-md p-3"
-        type="text"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </div>
   );
